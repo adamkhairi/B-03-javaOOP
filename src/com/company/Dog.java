a81ea3f2161c41b123b448228450fcf3ee27d5a2
adamkhairi <khairiadam2@gmail.com>
2020-12-08 14:54:05 +0100

    Teest

--- src/com/company/Dog.java
+++ src/com/company/Dog.java
@@ -1,20 +1,14 @@
 package com.company;
 
-// I-
-// public class Dog {
-//    public String name;
-//    public Dog(String name) {
-//        this.name = name;
-//    }
-//    public void bark() {
-//        System.out.println("Wwaaaarf !!");
-//    }
-//    public void eat() {
-//        System.out.println("The dog eat meat");
-//    }
-//}
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.swing.*;
+import java.io.File;
 
-// II-
+
+// I-
 public class Dog {
     public String name;
 
@@ -22,11 +16,64 @@ public class Dog {
         this.name = name;
     }
 
-    public void bark(String sound) {
-        System.out.println(sound);
+    //    public void playSound(String filename) {
+//        URL resource = ClassLoader.getSystemClassLoader().getResource(filename);
+//        try {
+//            final Clip clip = (Clip) AudioSystem.getLine(new Line.Info(Clip.class));
+//            clip.addLineListener(event -> {
+//                if (event.getType() == LineEvent.Type.STOP) {
+//                    clip.close();
+//                }
+//            });
+//            assert resource != null;
+//            clip.open(AudioSystem.getAudioInputStream(resource));
+//            clip.start();
+//        } catch (Exception e) {
+//            //LOGGER.error("Failed to play sound " + filename, e);
+//            System.out.println("Error");
+//        }
+//    }
+    public void playSound(String soundLocation) {
+        try {
+            File soundPath = new File(soundLocation);
+            if (soundPath.exists()) {
+
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(soundLocation);
+                Clip clip = AudioSystem.getClip();
+                clip.start();
+
+                JOptionPane.showMessageDialog(null, "Ok to Stop");
+            } else {
+                System.out.println("Invalid File");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void eat(String items) {
-        System.out.println(items);
+    public void bark() {
+        System.out.println("Wwaaaarf !!");
+    }
+
+    public void eat() {
+        System.out.println("The dog eat meat");
     }
 }
+
+// II-
+//public class Dog {
+//    public String name;
+//
+//    public Dog(String name) {
+//        this.name = name;
+//    }
+//
+//    public void bark(String sound) {
+//        System.out.println(sound);
+//    }
+//
+//    public void eat(String items) {
+//        System.out.println(items);
+//    }
+//
+//}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 //     I-
-//        Dog Rex = new Dog("Rex");
-//        Rex.bark();
-//        Rex.eat();
+        Dog Rex = new Dog("Rex");
+        Rex.bark();
+        Rex.eat();
 
-//        Dog Max = new Dog("Max");
-//        Max.bark();
-//        Max.eat();
+        Dog Max = new Dog("Max");
+        Max.bark();
+        Max.eat();
+        String filePath = "dogSound.mp3";
+        Max.playSound(filePath);
 
 //     II-
 //        Dog Rex = new Dog("Rex");
@@ -25,12 +27,11 @@ public class Main {
 //        Account acc1 = new Account("El Alami Hassan", "Safi", 14765.8);
 //        System.out.println(acc1.getName() + " habite à (" + acc1.getAddress() + ") - Infos du Solde " + acc1.getBalance() + " DH.");
 //
-        Account acc2 = new Account("Karimi Khalid", "Casablanca", 7654.0);
-        System.out.println(acc2.getName() + " habite à (" + acc2.getAddress() + ") - Infos du Solde " + acc2.getBalance() + " DH.");
-        System.out.println("Resultat Interet : " + acc2.calculateInteret(7) + " DH");
-        acc2.withdraw(1000);
-        System.out.println(acc2.getBalance());
-
+//        Account acc2 = new Account("Karimi Khalid", "Casablanca", 7654.0);
+//        System.out.println(acc2.getName() + " habite à (" + acc2.getAddress() + ") - Infos du Solde " + acc2.getBalance() + " DH.");
+//        System.out.println("Resultat Interet : " + acc2.calculateInteret(7) + " DH");
+//        acc2.withdraw(1000);
+//        System.out.println(acc2.getBalance());
 //     IV-
 
 
